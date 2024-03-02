b66f37be9052cf0207ea77a5d1adbbbd666d00dd
ShuvalovaEA <eshalos1207@gmail.com>
2024-03-02 18:23:34 +0300

    ads-1 upd

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,43 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
+#include <valarray>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    for (uint64_t i = 2; i<= sqrt(value); i++) {
+        if (value % i == 0) {
+            return false;
+        }
+        else {
+            return  true;
+        }
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t count = 1, num = 2;
+    while (count < n) {
+        num += 1;
+        if (checkPrime(num)) {
+            count += 1;
+        }
+    }
+    return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    while (checkPrime(value+1)) {
+        value += 1;
+    }
+    return  value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum = sum + i;
+        }
+    }
+    return sum;
 }
