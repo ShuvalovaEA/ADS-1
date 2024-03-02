63d89c129cdb62531b6a8f404cfcc71ba0eb8d36
ShuvalovaEA <eshalos1207@gmail.com>
2024-03-02 20:24:38 +0300

    ADS1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include <math.h>
+
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-    for (uint64_t i = 2; i <= sqrt(value); i++) {
+    for (uint64_t i = 2; i <= value / 2; i++) {
         if (value % i == 0) {
             return false;
         }
@@ -25,18 +25,18 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
     value += 1;
-    while (checkPrime(value)) {
+    while (!checkPrime(value)) {
         value += 1;
     }
     return  value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t summ = 0;
+    uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
-            summ += i;
+            sum += i;
         }
     }
-    return summ;
+    return sum;
 }
