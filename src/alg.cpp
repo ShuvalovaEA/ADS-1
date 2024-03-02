44a5783e46260d0d4f1e7ec8fc5e97ed5695fc53
ShuvalovaEA <eshalos1207@gmail.com>
2024-03-02 19:22:56 +0300

    ads-1 upd

--- src/alg.cpp
+++ src/alg.cpp
@@ -31,11 +31,11 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t summ = 0;
+    uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
-            summ = summ + i;
+            sum = sum + i;
         }
     }
-    return summ;
+    return sum;
 }
